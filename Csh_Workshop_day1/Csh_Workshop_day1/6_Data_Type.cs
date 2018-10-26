ddf05147c031a810236e6ee5b8a812c4a4744ae1
2014-3-60-001 <2014-3-60-001@cse.ewu>
2018-10-26 17:19:14 +0600

    day 1 last

--- Csh_Workshop_day1/Csh_Workshop_day1/6_Data_Type.cs
+++ Csh_Workshop_day1/Csh_Workshop_day1/6_Data_Type.cs
@@ -15,11 +15,36 @@ namespace Csh_Workshop_day1
             double c = 199.223123;      //64 bit
             decimal d = 192.321321321m; //128 bit
             char e = 'a';
+            int f = new int();
+            string g = "Hello World";
             Console.WriteLine("INT: " + a);
             Console.WriteLine("Float: " + b);
             Console.WriteLine("Double: " + c);
             Console.WriteLine("Decimal: " + d);
             Console.WriteLine("Char: " + e);
+            Console.WriteLine("New int: " + f);
+            Console.WriteLine("String: " + g);
+            Console.WriteLine("Taking input (Input int values)");
+            int h;
+            string i;
+            i = Console.ReadLine();
+            Console.WriteLine("As String: " +i);
+            h = Int32.Parse(i);
+            Console.WriteLine("As Int: " + h);
+            Console.WriteLine("Read input as int directly (Enter int value)");
+            h = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Read directly int: " + h);
+            Console.WriteLine("Input 2 values");
+            Console.WriteLine("TOGETHER: "+ (Convert.ToInt32(Console.ReadLine()) + Int32.Parse(Console.ReadLine())));
+            int j = 100;
+            short k = (short)j;
+            Console.WriteLine(k);
+
+            System.ConsoleKeyInfo ll = Console.ReadKey();
+            Console.WriteLine("OUTPUT " + ll.KeyChar);
+
+
+
             Console.ReadLine();
 
         }
