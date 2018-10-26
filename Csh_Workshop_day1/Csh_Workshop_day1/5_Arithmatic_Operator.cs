0da6279f29d32c615fc2f39b2c017a9bac845a31
2014-3-60-001 <2014-3-60-001@cse.ewu>
2018-10-26 16:38:41 +0600

    data type

--- Csh_Workshop_day1/Csh_Workshop_day1/5_Arithmatic_Operator.cs
+++ Csh_Workshop_day1/Csh_Workshop_day1/5_Arithmatic_Operator.cs
@@ -14,6 +14,25 @@ namespace Csh_Workshop_day1
             int e = d / c;
             int f = d % b;
             Console.WriteLine(d + " " + e + " " + f);
+            int g = 5;
+            g = g / 5;
+            Console.WriteLine(g);
+            Console.WriteLine("Post and Pre Increment");
+            int h = 5;
+            Console.WriteLine(h);
+            Console.WriteLine(h++);
+            Console.WriteLine(h);
+            Console.WriteLine(++h);
+            Console.WriteLine(h);
+            int k = 5;
+            int l = ++k;
+            Console.WriteLine("{0} {1}", k,l);
+            Console.WriteLine("%d %d", k, l);
+            int m = 5, n = 3;
+            int o = m++ + ++n + ++m;
+            Console.WriteLine(o);
+
+
             /*
             Console.WriteLine(d);
             Console.WriteLine(e);
---
+++ Csh_Workshop_day1/Csh_Workshop_day1/6_Data_Type.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csh_Workshop_day1
+{
+    class _6_Data_Type
+    {        
+        public static void Main(string[] args){
+
+            int a = 100;
+            float b = 54.21f;           //32 bit
+            double c = 199.223123;      //64 bit
+            decimal d = 192.321321321m; //128 bit
+            char e = 'a';
+            Console.WriteLine("INT: " + a);
+            Console.WriteLine("Float: " + b);
+            Console.WriteLine("Double: " + c);
+            Console.WriteLine("Decimal: " + d);
+            Console.WriteLine("Char: " + e);
+            Console.ReadLine();
+
+        }
+    }
+}
